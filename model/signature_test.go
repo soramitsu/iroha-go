95137eb8ea6062663d8cdddab4c447ef1b66c722
upamune <jajkeqos@gmail.com>
2017-06-06 16:42:21 +0900

    add model of signature

---
+++ model/signature_test.go
@@ -0,0 +1,25 @@
+package model
+
+import (
+	"testing"
+	"github.com/google/flatbuffers/go"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSignature(t *testing.T) {
+	sig := Signature{
+		Pubkey: []byte("this_is_test_pubkey"),
+		Sig: []byte("this_is_test_sig"),
+	}
+
+	builder := flatbuffers.NewBuilder(0)
+	sigOffset := sig.Serialize(builder)
+	builder.Finish(sigOffset)
+
+	b := builder.FinishedBytes()
+
+	sig2 := Signature{}
+	sig2.Deserialize(b)
+
+	assert.Equal(t, sig, sig2)
+}
