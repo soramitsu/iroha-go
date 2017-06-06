e95456c8d1f280c65b094e777cbb9e7f9f941997
upamune <jajkeqos@gmail.com>
2017-06-06 18:35:55 +0900

    fix signature's deserialize

--- model/signature_test.go
+++ model/signature_test.go
@@ -2,6 +2,7 @@ package model
 
 import (
 	"testing"
+
 	"github.com/google/flatbuffers/go"
 	"github.com/stretchr/testify/assert"
 )
@@ -9,7 +10,7 @@ import (
 func TestSignature(t *testing.T) {
 	sig := Signature{
 		Pubkey: []byte("this_is_test_pubkey"),
-		Sig: []byte("this_is_test_sig"),
+		Sig:    []byte("this_is_test_sig"),
 	}
 
 	builder := flatbuffers.NewBuilder(0)
@@ -19,7 +20,7 @@ func TestSignature(t *testing.T) {
 	b := builder.FinishedBytes()
 
 	sig2 := Signature{}
-	sig2.Deserialize(b)
+	sig2.Deserialize(b, 0)
 
 	assert.Equal(t, sig, sig2)
 }
